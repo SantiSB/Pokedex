5b71d81bc8231c57d661aed7b098988331c772ef
santiagoprueba <santiago@gmail.com>
2022-08-25 16:34:45 -0500

    integration of redux and redux hooks

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,6 @@
-import React, {useEffect, useState} from "react";
+import React, { useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { setPokemons } from "./actions";
 import CssBaseline from "@mui/material/CssBaseline";
 import { createTheme, ThemeProvider } from "@mui/material/styles";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
@@ -17,33 +19,31 @@ const theme = createTheme({
 });
 
 function App() {
-
-  const [pokemons, setPokemons] = useState([])
+  const pokemons = useSelector((state) => state.pokemons);
+  const dispatch = useDispatch();
 
   useEffect(() => {
-    const fetchPokemons = async() => {
+    const fetchPokemons = async () => {
       const pokemonsRes = await getPokemon();
-      setPokemons(pokemonsRes)
+      dispatch(setPokemons(pokemonsRes));
     };
     fetchPokemons();
   }, []);
 
-  
-
   return (
-    <div style={{ overflow: "hidden"}}>
-    <ThemeProvider theme={theme}>
-      <CssBaseline />
-      <BrowserRouter>
-        <Layout>
-          <Routes>
-            <Route path="/" element={<Home pokemons={pokemons}/>} />
-            <Route path="/pokemon" element={<Details />} />
-            <Route path="*" element={<NotFound />} />
-          </Routes>
-        </Layout>
-      </BrowserRouter>
-    </ThemeProvider>
+    <div style={{ overflow: "hidden" }}>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <BrowserRouter>
+          <Layout>
+            <Routes>
+              <Route path="/" element={<Home pokemons={pokemons} />} />
+              <Route path="/pokemon" element={<Details />} />
+              <Route path="*" element={<NotFound />} />
+            </Routes>
+          </Layout>
+        </BrowserRouter>
+      </ThemeProvider>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,6 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
+import { pokemonsReducer } from "./reducers/pokemons";
+import { Provider } from "react-redux";
+import { legacy_createStore as createStore } from "redux";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<App />);
+const store = createStore(pokemonsReducer);
+root.render(
+  <React.StrictMode>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </React.StrictMode>
+);
